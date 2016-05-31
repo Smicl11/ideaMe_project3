4546ef7c1a0a8edc2c5698a2e44da094b34be1dd
smicl11 <bryan.smith094@gmail.com>
2016-05-31 11:45:51 -0700

    updated styling for homepage and added image for jumbotron

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   #user routes
   root to: "users#index"
-  resources :users
+  get "/users", to: "users#index", as: "users"
+  get "/users/new", to: "users#new", as: "new_user"
+  post "/users", to: "users#create", as: "create_user"
+  get "/users/:id", to: "users#show", as: "user"
+  get "users/:id/edit", to: "users#edit", as:"user_edit"
+  patch "/users/:id", to: "users#update", as: "update_user"
+  delete '/users/:id', to: "users#destroy", as: "destroy_user"
 
   #Sessions
   get "/login", to: "sessions#new"
