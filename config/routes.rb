ccbd5922f19a51f61c5bd59681fa75226751f289
smicl11 <bryan.smith094@gmail.com>
2016-05-31 10:58:07 -0700

    added idea association to user

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,13 @@ Rails.application.routes.draw do
 
   #user routes
   root to: "users#index"
-  get "/users", to: "users#index", as: "users"
-  get "/users/new", to: "users#new", as: "new_user"
-  post "/users", to: "users#create", as: "create_user"
-  get "/users/:id", to: "users#show", as: "user"
-  get "users/:id/edit", to: "users#edit", as:"user_edit"
-  patch "/users/:id", to: "users#update", as: "update_user"
-  delete '/users/:id', to: "users#destroy", as: "destroy_user"
+  resources :users
 
   #Sessions
   get "/login", to: "sessions#new"
   get "/logout", to: "sessions#destroy"
   post "/sessions", to: "sessions#create"
 
+  #Idea routes
+  resources :ideas
 end
