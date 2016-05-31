f7cc6c111a272907ad52c1127f072779cf224885
smicl11 <bryan.smith094@gmail.com>
2016-05-31 12:53:55 -0700

    created idea views and form partial. updated idea routes to not use resource for better control

--- config/routes.rb
+++ config/routes.rb
@@ -16,5 +16,11 @@ Rails.application.routes.draw do
   post "/sessions", to: "sessions#create"
 
   #Idea routes
-  resources :ideas
+  get "/ideas", to: "ideas#index", as: "ideas"
+  get "/ideas/new", to: "ideas#new", as: "new_idea"
+  post "/ideas", to: "ideas#create", as: "create_idea"
+  get "/ideas/:id", to: "ideas#show", as: "idea"
+  get "/ideas/:id/edit", to: "ideas#edit", as: "edit_idea"
+  patch "/ideas/:id", to: "ideas#update", as: "update_idea"
+  delete "/ideas/:id", to: "ideas#destroy", as: "delete_idea"
 end
