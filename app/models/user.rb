bdc056dc0ca0fe1e1915a45354fc212a9cceb43c
smicl11 <bryan.smith094@gmail.com>
2016-06-02 15:48:12 -0700

    added validations to email, frist name, and password to ensure all are present. set character min on password and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :ideas, dependent: :destroy
+  validates :email, presence: true, uniqueness: true, length: { minimum: 6 }
+  validates :first_name, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
