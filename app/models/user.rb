5b6487af8798e1aece73be09ea6b28a1e0207559
smicl11 <bryan.smith094@gmail.com>
2016-07-06 17:24:04 -0700

    added attached file and validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :ideas, dependent: :destroy
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
   validates :email, presence: true, uniqueness: true, length: { minimum: 6 }
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validates :first_name, :last_name, presence: true
   validates :password, presence: true, length: { minimum: 6 }, :on => :create
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 
   def self.confirm(params)
     @user = User.find_by({email: params[:email]})
